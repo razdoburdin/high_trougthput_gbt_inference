faacab1f0fd637a066d1f353ba66c5f51254326c
Dmitry Razdoburdin <>
2026-05-12 01:35:21 -0700

    log ram consumption

--- bench.sh
+++ bench.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 for fw in xgboost daal4py treelite onnx xgboost_sycl; do
-# for fw in treelite onnx; do
-    python run_inference.py --framework=$fw
+    /usr/bin/time python run_inference.py --framework=$fw &> $fw.log
 done
